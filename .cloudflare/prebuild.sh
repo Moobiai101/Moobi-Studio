1a2e25a2c026fd076c38e89e73af2f611ca8558a
moobi labs <moobilabs1@gmail.com>
2025-04-27 00:04:07 +0530

    whatever

--- .cloudflare/prebuild.sh
+++ .cloudflare/prebuild.sh
@@ -3,6 +3,6 @@
 # Clean webpack cache to avoid large files
 echo "Cleaning webpack cache..."
 rm -rf .next/cache
-
+ 
 # Create an empty .cache directory if it doesn't exist
 mkdir -p .next/cache/webpack 
