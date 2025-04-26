b24692b665fa0f2ad9c477fb8f336d300f90dc69
moobi labs <moobilabs1@gmail.com>
2025-04-26 23:57:51 +0530

    feat: Remove dist from git and add to .gitignore

--- dist/static/chunks/src_app_studios_image-studio_page_tsx_54d64407._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_e588389f._.js",
-  "static/chunks/node_modules_b5419506._.js"
-],
-    source: "dynamic"
-});
